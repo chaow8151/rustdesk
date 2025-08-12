13dcc99ac4fad6fdd19f27b36697234a86511b92
kuda0313 <kuda0313@gmail.com>
2025-08-12 14:14:03 +0800

    fix all 0812

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["lecheng.mooo.com"];
+pub const RS_PUB_KEY: &str = "48I4fjdd2uCQJHQRDF1OmPd9sOafawc8rqVIheoCRzE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
@@ -2374,13 +2374,15 @@ pub fn is_disable_installation() -> bool {
 // flutter: flutter/lib/common.dart -> option2bool()
 // sciter: Does not have the function, but it should be kept the same.
 pub fn option2bool(option: &str, value: &str) -> bool {
-    if option.starts_with("enable-") {
+    if option == "direct-server" ||
+       option == "allow-remote-config-modification" ||
+       option == "allow-numeric-one-time-password" {
         value != "N"
-    } else if option.starts_with("allow-")
-        || option == "stop-service"
-        || option == keys::OPTION_DIRECT_SERVER
-        || option == "force-always-relay"
-    {
+    } else if option.starts_with("enable-") {
+        value != "N"
+    } else if option.starts_with("allow-") ||
+              option == "stop-service" ||
+              option == "force-always-relay" {
         value == "Y"
     } else {
         value != "N"
